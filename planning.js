024a65e40119e1b57a52a8af4bdf0c3f2803c641
Aashita-Goyal <aashitagoyal99@gmail.com>
2021-07-16 19:27:47 +0530

    navbar

---
+++ planning.js
@@ -0,0 +1,28 @@
+//Book my show planning
+
+
+
+//we need resusable carousel
+   //  hero carousel
+   //  common carousel
+   //  cast and crew samll carousel
+
+
+//Pages
+   //  home page
+   //  movie page
+   //  categories page
+   //  auth page
+   //  razorpay payment gateway
+
+
+
+//Plan - How are we building it
+  //starting with layout
+  //then routes
+  //then pages
+  //and components
+
+
+  
+//HOC - Higher Order Components
